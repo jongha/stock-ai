f213130b03d794bce89467b60aa7ba70edfb57b2
Jong-Ha Ahn <jongha.ahn@mrlatte.net>
2016-11-25 00:39:20 +0000

    Fix FileNotFoundError for temp direcotry (wrong path)

--- config.py
+++ config.py
@@ -2,7 +2,15 @@
 import os
 import tempfile
 
+APP_NAME = 'stock-ai'
 REQUEST_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'
 
 DATA_DISCOUNT_RATE = 0.1 # percent
-DATA_PATH = tempfile.gettempdir() # os.path.dirname(os.path.abspath(__file__))
+DATA_BASE_PATH = tempfile.gettempdir() # os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = os.path.join(DATA_BASE_PATH, APP_NAME)
+
+if not os.path.exists(DATA_PATH):
+  try:
+    os.makedirs(DATA_PATH)
+  except:
+    pass
--- modules/base.py
+++ modules/base.py
@@ -8,13 +8,7 @@ import re
 import pickle
 
 def get_file_path(code):
-  if not os.path.exists(config.DATA_PATH):
-    try:
-      os.makedirs(config.DATA_PATH)
-    except:
-      pass
-
-  return os.path.join(config.DATA_PATH, 'data', code + '.pkl')
+  return os.path.join(config.DATA_PATH, code + '.pkl')
 
 def download(code, year1, month1, day1, year2, month2, day2):
   start = datetime.datetime(year1, month1, day1)
